e4ac9b3208db8d530a9fd3cb8e3481f6b8f8aaed
bzy-080408 <63627217+bzy-080408@users.noreply.github.com>
2022-12-29 06:58:48 +0800

    Update rules.mk

--- dts/rules.mk
+++ dts/rules.mk
@@ -11,5 +11,20 @@ DTBS += \
 	$(LOCAL_DIR)/can-l01-va.dtb \
 	$(LOCAL_DIR)/can-l01-vb.dtb \
 	$(LOCAL_DIR)/can-l01-vc.dtb \
-	$(LOCAL_DIR)/can-l01-vd.dtb
+	$(LOCAL_DIR)/can-l01-vd.dtb \
+	$(LOCAL_DIR)/can-l02-va.dtb \
+	$(LOCAL_DIR)/can-l02-vb.dtb \
+	$(LOCAL_DIR)/can-l03-va.dtb \
+	$(LOCAL_DIR)/can-l03-vb.dtb \
+	$(LOCAL_DIR)/can-l03-vc.dtb \
+	$(LOCAL_DIR)/can-tl00-va.dtb \
+	$(LOCAL_DIR)/mla-al00-vb.dtb \
+	$(LOCAL_DIR)/mla-al00-vc.dtb \
+	$(LOCAL_DIR)/mla-al00-vd.dtb \
+	$(LOCAL_DIR)/mla-al00-ve.dtb \
+	$(LOCAL_DIR)/mla-tl00-va.dtb \
+	$(LOCAL_DIR)/mla-l01-va.dtb \
+	$(LOCAL_DIR)/mla-l01-vb.dtb \
+	$(LOCAL_DIR)/mla-l02-va.dtb \
+	$(LOCAL_DIR)/mla-l03-va.dtb
 endif

ecd4a84d8b949c5286383fdba56ef5b6dfce4eda
bzy-080408 <63627217+bzy-080408@users.noreply.github.com>
2022-12-28 18:08:11 +0800

    Update rules.mk

--- dts/rules.mk
+++ dts/rules.mk
@@ -3,5 +3,13 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 ifeq ($(PROJECT), msm8953-secondary)
 DTBS += \
 	$(LOCAL_DIR)/msm8953-huawei-hwcan.dtb \
-	$(LOCAL_DIR)/can-va.dtb \
+	$(LOCAL_DIR)/can-al00-va.dtb \
+	$(LOCAL_DIR)/can-al00-vb.dtb \
+	$(LOCAL_DIR)/can-al00-vc.dtb \
+	$(LOCAL_DIR)/can-al00-vd.dtb \
+	$(LOCAL_DIR)/can-cl00-va.dtb \
+	$(LOCAL_DIR)/can-l01-va.dtb \
+	$(LOCAL_DIR)/can-l01-vb.dtb \
+	$(LOCAL_DIR)/can-l01-vc.dtb \
+	$(LOCAL_DIR)/can-l01-vd.dtb
 endif

9b79f074cc1e302217abe78fed92514923252d8a
bzy-080408 <63627217+bzy-080408@users.noreply.github.com>
2023-01-21 21:30:28 +0800

    Update rules.mk

--- dts/rules.mk
+++ dts/rules.mk
@@ -29,5 +29,6 @@ DTBS += \
 	$(LOCAL_DIR)/xiaomi_oxyged.dtb \
 	$(LOCAL_DIR)/xiaomi-vince.dtb \
 	$(LOCAL_DIR)/xiaomi-daisy.dtb \
-	$(LOCAL_DIR)/xiaomi-tissot.dtb
+	$(LOCAL_DIR)/xiaomi-tissot.dtb \
+	$(LOCAL_DIR)/meizu-m1721.dtb
 endif

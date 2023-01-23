69711d556dbafd1d4cff5351ab5f7952a0b68465
bzy-080408 <63627217+bzy-080408@users.noreply.github.com>
2023-01-23 11:18:35 +0800

    change projcet to msm8937-secondary

--- dts/rules.mk
+++ dts/rules.mk
@@ -1,37 +1,8 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-ifeq ($(PROJECT), msm8953-secondary)
+ifeq ($(PROJECT), msm8937-secondary)
 DTBS += \
 	$(LOCAL_DIR)/can-al00-va.dtb \
-	$(LOCAL_DIR)/can-al00-vb.dtb \
-	$(LOCAL_DIR)/can-al00-vc.dtb \
-	$(LOCAL_DIR)/can-al00-vd.dtb \
-	$(LOCAL_DIR)/can-cl00-va.dtb \
-	$(LOCAL_DIR)/can-l01-va.dtb \
-	$(LOCAL_DIR)/can-l01-vb.dtb \
-	$(LOCAL_DIR)/can-l01-vc.dtb \
-	$(LOCAL_DIR)/can-l01-vd.dtb \
-	$(LOCAL_DIR)/can-l02-va.dtb \
-	$(LOCAL_DIR)/can-l02-vb.dtb \
-	$(LOCAL_DIR)/can-l03-va.dtb \
-	$(LOCAL_DIR)/can-l03-vb.dtb \
-	$(LOCAL_DIR)/can-l03-vc.dtb \
-	$(LOCAL_DIR)/can-tl00-va.dtb \
-	$(LOCAL_DIR)/mla-al00-vb.dtb \
-	$(LOCAL_DIR)/mla-al00-vc.dtb \
-	$(LOCAL_DIR)/mla-al00-vd.dtb \
-	$(LOCAL_DIR)/mla-al00-ve.dtb \
-	$(LOCAL_DIR)/mla-tl00-va.dtb \
-	$(LOCAL_DIR)/mla-l01-va.dtb \
-	$(LOCAL_DIR)/mla-l01-vb.dtb \
-	$(LOCAL_DIR)/mla-l02-va.dtb \
-	$(LOCAL_DIR)/mla-l03-va.dtb \
-	$(LOCAL_DIR)/xiaomi_oxyged.dtb \
-	$(LOCAL_DIR)/xiaomi-vince.dtb \
-	$(LOCAL_DIR)/xiaomi-daisy.dtb \
-	$(LOCAL_DIR)/xiaomi-tissot.dtb \
-	$(LOCAL_DIR)/lenovo-kuntao-p0.dtb \
-	$(LOCAL_DIR)/lenovo-kuntao-p2b1.dtb \
-	$(LOCAL_DIR)/meizu-m1721.dtb 
+
 
 endif

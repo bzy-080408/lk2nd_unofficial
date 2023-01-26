5987aee28b17a394d11c8e0facd67d87fad707ef
bzy-080408 <63627217+bzy-080408@users.noreply.github.com>
2023-01-26 09:41:59 +0000

    fix build error

--- dts/rules.mk
+++ dts/rules.mk
@@ -6,7 +6,7 @@ DTBS += \
 	$(LOCAL_DIR)/BAH-L01.dtb \
 	$(LOCAL_DIR)/BAH-L09.dtb \
 	$(LOCAL_DIR)/CPN-AL00.dtb \
-	$(LOCAL_DIR)/APN-L01.dtb \
+	$(LOCAL_DIR)/CPN-L01.dtb \
 	$(LOCAL_DIR)/CPN-L09.dtb \
 	$(LOCAL_DIR)/CPN-L0J.dtb \
 	$(LOCAL_DIR)/CPN-W09.dtb 
